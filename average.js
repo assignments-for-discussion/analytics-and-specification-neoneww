e94ac20cfd1cfc20986dfb487c9e13105ce231fa
sourav sharma <souravat@outlook.com>
2021-12-10 19:31:42 +0530

    update

--- average.js
+++ average.js
@@ -8,8 +8,10 @@ const outlierDetector = (collection) => {
 	let q1, q3;
 	const sortedCollection = collection.slice().sort((a, b) => a - b);
 	if (((size - 1) / 4) % 1 === 0 || (size / 4) % 1 === 0) {
-		q1 =(1 / 2)*(sortedCollection[Math.floor(size / 4) - 1]+sortedCollection[Math.floor(size / 4)]);
-		q3 =(1 / 2)*(sortedCollection[Math.ceil((size * 3) / 4) - 1]+sortedCollection[Math.ceil((size * 3) / 4)]);
+		q1 =(1 / 2)*(sortedCollection[Math.floor(size / 4) - 1]
+		+sortedCollection[Math.floor(size / 4)]);
+		q3 =(1 / 2)*(sortedCollection[Math.ceil((size * 3) / 4) - 1]
+		+sortedCollection[Math.ceil((size * 3) / 4)]);
 	}
 	else
 	{
